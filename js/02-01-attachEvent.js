ffac5b9f21e637de0a404f8c0be67ec0d4a2dacf
kuas1091104337 <kuas1091104337@gmail.com>
2023-02-06 23:37:34 +0800

    ch 02-1

--- js/02-01-attachEvent.js
+++ js/02-01-attachEvent.js
@@ -1,25 +1,15 @@
 $(function(){
-
   function clickHandler(){
     alert('CLICK')
   }
-
   $('.on').click(function(){
-
     $('img').on('click',clickHandler)
-
   })
-
   $('.off').click(function(){
-
     $('img').off('click',clickHandler)
-
   })
-
   $('.one').click(function(){
-
     $('img').one('click',clickHandler)
-
   })
-
-})
+  clickHandler()
+});
